13b6a1f30d126f5bf7ff1caef5e2df9feebaac0b
ezevkyx <48946650+ezevkyx@users.noreply.github.com>
2021-04-01 00:16:53 +0200

    Update script.js

--- Project/Datengrafik/script.js
+++ Project/Datengrafik/script.js
@@ -1,9 +1,11 @@
 function showDoku() {
     document.getElementById("dokuPanel").style.visibility = "visible";
+    document.getElementById("dokuPanel").style.display = "block";
+    // console.log("block");
 }
 hideDoku();
 
 function hideDoku() {
-    document.getElementById("dokuPanel").style.visibility = "hidden";
     document.getElementById("dokuPanel").style.display = "none";
+    // console.log("hide");
 }
