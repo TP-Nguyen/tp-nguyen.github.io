789487c11d2f4456dda01fd305a9c5a5e4e48f97
ezevkyx <48946650+ezevkyx@users.noreply.github.com>
2023-10-18 18:13:10 +0200

    add deploy workflow

---
+++ .vite/deps/vuetify_lib_iconsets_mdi.js
@@ -0,0 +1,11 @@
+import {
+  aliases,
+  mdi
+} from "./chunk-IZPV4N4X.js";
+import "./chunk-JBYAEPPU.js";
+import "./chunk-DABAD5JF.js";
+export {
+  aliases,
+  mdi
+};
+//# sourceMappingURL=vuetify_lib_iconsets_mdi.js.map
